1d5a00a37f14a224f3bbffd9da3c95e56bb3fc53
Gregg Meluski <gmeluski@gmail.com>
2014-11-03 22:24:05 -0500

    fixed this shady framing situation

--- complete.R
+++ complete.R
@@ -1,12 +1,15 @@
 complete <- function (directory, ids) {
-    reportFrame <- data.frame()
+    finalFrame <- data.frame()
     reportFiles <- list.files(directory, full.name=TRUE)
     for (i in ids) {
         # get the number of obeserved cases, sum the ones up 
         # complete cases have both sulfate and nitrate columns
+        reportFrame <- data.frame()
         currentFile = reportFiles[i]
         reportFrame <- rbind(reportFrame, read.csv(currentFile))    
         good <- reportFrame[complete.cases(reportFrame),]
-        print(nrow(good))
+        finalFrame <- rbind(finalFrame, c(i, nrow(good)))
     }
+    colnames(finalFrame) <- c('id', 'nobs')
+    print(finalFrame)
 }
