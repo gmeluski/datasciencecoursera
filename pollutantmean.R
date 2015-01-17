0bbd2330d87885252cf5aaf82b45a1c7f7576017
Gregg Meluski <gmeluski@gmail.com>
2015-01-17 13:56:35 -0500

    update to include variable names for the columns

--- pollutantmean.R
+++ pollutantmean.R
@@ -2,12 +2,13 @@
 # @param {Character Vector} the name of the pollutant to search for
 # @param {Integer Vector} the monitor id numbers to be used
 # @return {Number} the mean of the pollutant across all monitors
-pollutantMean <- function (directory, pollutantToFind, ids) {
+pollutantmean <- function (directory, pollutantToFind, ids) {
+    # empty data frame
     pollutionFrame <- data.frame()
     pollutionFiles <- list.files(directory, full.name=TRUE)
     for (i in ids) {
         pollutionFrame <- rbind(pollutionFrame, read.csv(pollutionFiles[i]))    
     }
     
-    mean(pollutionFrame$sulfate, na.rm=TRUE)
+    mean(pollutionFrame[,pollutantToFind], na.rm=TRUE)
 }
