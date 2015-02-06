781ea1738850a69361e7e510e91240e743d449ed
Gregg Meluski <gmeluski@gmail.com>
2015-02-06 09:01:02 -0500

    added in 2 other functions

--- best.R
+++ best.R
@@ -1,4 +1,22 @@
 best <- function (state, outcome) {
-    #Read outcome data
+  #Read outcome data
+  # Check that the state and outcome are valid
+  # Return hospital name in that state with the lowest 30-day death rate
+}
 
+
+rankhospital <- function (state, outcome, num='best') {
+  # read outcome data
+  # Check that the state and outcome are valid
+  # Return the hospital name in that state with given rank 30-day death rate
+
+}
+
+
+rankall <- function (outcome, num = 'best') {
+  # read outcome data
+  # check that state and outcome are valid
+  # for each state, find the hospital of the given rank
+  # return a data frame with the hospital names and the abbreviated state name
+  
 }
