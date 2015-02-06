995b20c7a78405892112df56be70e2bd83347a69
Gregg Meluski <gmeluski@gmail.com>
2015-02-06 08:53:07 -0500

    added in default

--- pollutantmean.R
+++ pollutantmean.R
@@ -2,7 +2,7 @@
 # @param {Character Vector} the name of the pollutant to search for
 # @param {Integer Vector} the monitor id numbers to be used
 # @return {Number} the mean of the pollutant across all monitors
-pollutantmean <- function (directory, pollutantToFind, ids) {
+pollutantmean <- function (directory, pollutantToFind, ids=1:332) {
     # empty data frame
     pollutionFrame <- data.frame()
     pollutionFiles <- list.files(directory, full.name=TRUE)
