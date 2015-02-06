b8ada6e0ed35a98648a359950a14f6a588bd91d6
Gregg Meluski <gmeluski@gmail.com>
2015-02-06 09:08:22 -0500

    fixed up files for programming assingment 4

--- best.R
+++ best.R
@@ -5,12 +5,3 @@ best <- function (state, outcome) {
   # Return hospital name in that state with the lowest 30-day death rate
 }
 
-
-
-rankall <- function (outcome, num = 'best') {
-  # read outcome data
-  # check that state and outcome are valid
-  # for each state, find the hospital of the given rank
-  # return a data frame with the hospital names and the abbreviated state name
-  
-}
---
+++ rankall.R
@@ -0,0 +1,7 @@
+rankall <- function (outcome, num = 'best') {
+  # read outcome data
+  # check that state and outcome are valid
+  # for each state, find the hospital of the given rank
+  # return a data frame with the hospital names and the abbreviated state name
+  
+}
