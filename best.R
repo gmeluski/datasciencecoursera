ff1116a33e38f2921d230609c0e7c8ce7f0d14f1
Gregg Meluski <gmeluski@gmail.com>
2015-02-06 09:04:07 -0500

    splliting up files as appropriate

--- best.R
+++ best.R
@@ -1,16 +1,10 @@
 best <- function (state, outcome) {
+  # The hospital name is the name provided in the Hospital.Name variable
   #Read outcome data
   # Check that the state and outcome are valid
   # Return hospital name in that state with the lowest 30-day death rate
 }
 
-
-rankhospital <- function (state, outcome, num='best') {
-  # read outcome data
-  # Check that the state and outcome are valid
-  # Return the hospital name in that state with given rank 30-day death rate
-
-}
 
 
 rankall <- function (outcome, num = 'best') {
---
+++ rankhospital.R
@@ -0,0 +1,7 @@
+rankhospital <- function (state, outcome, num='best') {
+  # read outcome data
+  # Check that the state and outcome are valid
+  # Return the hospital name in that state with given rank 30-day death rate
+
+}
+
