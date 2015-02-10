ace11ba1a42909c06b7e2a4e3cb2069da4bfed19
Gregg Meluski <gmeluski@gmail.com>
2015-02-09 21:37:54 -0500

    add in the two new hospital files

--- rankall.R
+++ rankall.R
@@ -1,7 +1,40 @@
 rankall <- function (outcome, num = 'best') {
   # read outcome data
-  # check that state and outcome are valid
-  # for each state, find the hospital of the given rank
-  # return a data frame with the hospital names and the abbreviated state name
+  outcomeData <- read.csv('outcome-of-care-measures.csv', colClasses = 'character');
   
+  # Check that the state and outcome are valid
+  validOutcomes <- c('heart attack', 'heart failure', 'pneumonia');
+  validStates <- sort(unique(outcomeData[, 7]))
+
+  # Check that the state and outcome are valid
+  if (!outcome %in% validOutcomes) {
+    stop('invalid outcome');
+  }
+
+
+  if (!state %in% validStates) {
+    stop('invalid state');
+  }
+
+  columnNames <- c('Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack', 'Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure', 'Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia')
+  columnPosition <- match(outcome, validOutcomes);
+  fullColumnName <- columnNames[columnPosition];
+
+  for (index in seq_along(validStates)) {
+    stateData <- outcomeData[outcomeData$State == validStates[index], ]
+    sortedStateData <- stateData[order(as.numeric(stateData[[fullColumnName]]), stateData[['Hospital.Name']], decreasing=FALSE, na.last=NA), ]
+
+    this.num = num
+    if (this.num == 'best' ) {
+      this.num = 1
+    }
+
+    if (this.num == 'worst') {
+      this.num = nrow(sortedStateData)
+    }
+
+    hospital[index] <- sortedStateData[this.num, 'Hospital.Name']
+  }
+
+  data.frame(hospital=hospital, state=validStates, row.names = validStates)
 }
--- rankhospital.R
+++ rankhospital.R
@@ -1,7 +1,36 @@
 rankhospital <- function (state, outcome, num='best') {
   # read outcome data
-  # Check that the state and outcome are valid
-  # Return the hospital name in that state with given rank 30-day death rate
+  outcomeData <- read.csv('outcome-of-care-measures.csv', colClasses = 'character');
 
+  # Check that the state and outcome are valid
+  validOutcomes <- c('heart attack', 'heart failure', 'pneumonia');
+  validStates <- unique(outcomeData[, 7])
+
+  # Check that the state and outcome are valid
+  if (!outcome %in% validOutcomes) {
+    stop('invalid outcome');
+  }
+
+
+  if (!state %in% validStates) {
+    stop('invalid state');
+  }
+
+  columnNames <- c('Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack', 'Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure', 'Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia')
+  columnPosition <- match(outcome, validOutcomes);
+  fullColumnName <- columnNames[columnPosition];
+
+  # Return the hospital name in that state with given rank 30-day death rate
+  chosenStateInfo <- outcomeData[outcomeData$State == state]; 
+  dataByState <- chosenStateInfo[order(as.numeric(chosenStateInfo[[fullColumnName]]), chosenStateInfo[['Hospital.Name']], decreasing=FALSE, na.last=NA)]
+
+  if (num == 'best') {
+    num = 1
+  }
+  if (num == 'worst') {
+    num = nrow(dataByState)
+  }
+
+  dataByState[num, 'Hospital.Name']
 }
 
